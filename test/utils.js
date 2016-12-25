62a8f6b0137661ccbb1e2f28b3e87245f1415044
James Conkling <james.lane.conkling@gmail.com>
2016-12-25 01:17:57 -0500

    add expandPath util

--- test/utils.js
+++ test/utils.js
@@ -1,8 +1,9 @@
 const tape = require('tape');
 const walkTree = require('../utils').walkTree;
-const extractPathFromTree = require('../utils').extractPathFromTree;
 const pathValue2Tree = require('../utils').pathValue2Tree;
 const pathValues2JSONGraphEnvelope = require('../utils').pathValues2JSONGraphEnvelope;
+const expandPath = require('../utils').expandPath;
+const extractPathFromTree = require('../utils').extractPathFromTree;
 
 
 tape('walkTree - Should return the value in the tree at the specified value', t => {
@@ -30,56 +31,6 @@ tape('walkTree - Should return undefined if the path does exist in the tree', t 
   const expected = undefined;
 
   t.equal(walkTree(path, tree), expected);
-});
-
-
-tape('extractPathFromTree - Should extract sub tree from tree that contains path', t => {
-  t.plan(1);
-
-  const tree = {
-    people: {
-      '1': { name: 'Tom', age: 28 },
-      '2': { name: 'Dick' },
-    }
-  };
-  const path = ['people', 1, 'age'];
-  const expected = {
-    people: { 1: { age: 28 } }
-  };
-
-  t.deepEqual(extractPathFromTree(path, tree), expected);
-});
-// Unclear what the expected behavior should be.  calling GET w/ the below path on a model w/ local cache returns the following
-tape('extractPathFromTree - Should return sub tree with empty atom leaf node for incomplete path', t => {
-  t.plan(1);
-
-  const tree = {
-    people: {
-      '1': { name: 'Tom', age: 28 },
-    }
-  };
-  const path = ['people', 1];
-  const expected = {
-    people: { 1: { $type: 'atom' } }
-  };
-
-  t.deepEqual(extractPathFromTree(path, tree), expected);
-});
-// Unclear what the expected behavior should be.  calling GET w/ the below path on a model w/ local cache returns the following
-tape('extractPathFromTree - Should return sub tree with empty atom leaf node for non-existant path in tree', t => {
-  t.plan(1);
-
-  const tree = {
-    people: {
-      '1': { name: 'Tom', age: 28 },
-    }
-  };
-  const path = ['places', 1, 'name'];
-  const expected = {
-    places: { 1: { name: { $type: 'atom' } } }
-  };
-
-  t.deepEqual(extractPathFromTree(path, tree), expected);
 });
 
 
@@ -158,10 +109,96 @@ tape('pathValues2JSONGraphEnvelope - Should convert multiple pathValues to a jso
       }
     },
     paths: [
-      ['people', 2, ['age', 'name']],
+      ['people', 2, ['age', 'name']], // todo, collapsed keySets probably don't guarantee order
       ['people', 3, 'name']
     ]
   };
 
   t.deepEqual(pathValues2JSONGraphEnvelope(pathValues), expected);
+});
+
+
+tape('expandPath - Should expand a simple path into a list of paths', t => {
+  t.plan(1);
+
+  const path = ['people', 1, 'age'];
+  const expected = [['people', 1, 'age']];
+
+  t.deepEqual(expandPath(path), expected);
+});
+tape('expandPath - Should expand a path with a single keySet of length 2 into a list of 2 paths', t => {
+  t.plan(1);
+
+  const path = ['people', 1, ['age', 'name']];
+  const expected = [
+    ['people', 1, 'age'],
+    ['people', 1, 'name']
+  ];
+
+  t.deepEqual(expandPath(path), expected);
+});
+tape('expandPath - Should expand a path with two keySets of length 2 and 3 into a list of 6 paths', t => {
+  t.plan(1);
+
+  const path = ['people', [1, 2, 4], ['age', 'name']];
+  const expected = [
+    ['people', 1, 'age'],
+    ['people', 2, 'age'],
+    ['people', 4, 'age'],
+    ['people', 1, 'name'],
+    ['people', 2, 'name'],
+    ['people', 4, 'name']
+  ];
+
+  t.deepEqual(expandPath(path), expected);
+});
+
+
+tape('extractPathFromTree - Should extract sub tree from tree that contains path', t => {
+  t.plan(1);
+
+  const tree = {
+    people: {
+      '1': { name: 'Tom', age: 28 },
+      '2': { name: 'Dick' },
+    }
+  };
+  const path = ['people', 1, 'age'];
+  const expected = {
+    people: { 1: { age: 28 } }
+  };
+
+  t.deepEqual(extractPathFromTree(path, tree), expected);
+});
+// Unclear what the expected behavior should be.  calling GET w/ the below path on a model w/ local cache returns the following
+tape('extractPathFromTree - Should return sub tree with empty atom leaf node for incomplete path', t => {
+  t.plan(1);
+
+  const tree = {
+    people: {
+      '1': { name: 'Tom', age: 28 },
+    }
+  };
+  const path = ['people', 1];
+  const expected = {
+    people: { 1: { $type: 'atom' } }
+  };
+
+  t.deepEqual(extractPathFromTree(path, tree), expected);
+});
+// Unclear what the expected behavior should be.  calling GET w/ the below path on a model w/ local cache returns the following
+tape('extractPathFromTree - Should return sub tree with empty atom leaf node for non-existant path in tree', t => {
+  t.plan(1);
+
+  const tree = {
+    people: {
+      '1': { name: 'Tom', age: 28 },
+    }
+  };
+  const path = ['places', 1, 'name'];
+  const expected = {
+    places: { 1: { name: { $type: 'atom' } } }
+  };
+
+  t.deepEqual(extractPathFromTree(path, tree), expected);
 });
--- utils.js
+++ utils.js
@@ -1,4 +1,8 @@
+// TODO:
+// * add type defs
+// * test these against refs
 const collapse = require('falcor-path-utils').collapse;
+
 
 const walkTree = (path, tree) => {
   if (path.length === 1) {
@@ -9,6 +13,53 @@ const walkTree = (path, tree) => {
   }
   return undefined;
 };
+
+
+// TODO - make tail recursive?
+const pathValue2Tree = (pathValue, tree = {}) => {
+  if (pathValue.path.length === 1) {
+    return Object.assign(tree, {
+      [pathValue.path[0]]: pathValue.value
+    });
+  }
+
+  return Object.assign(tree, {
+    [pathValue.path[0]]: pathValue2Tree({ path: pathValue.path.slice(1), value: pathValue.value }, tree[pathValue.path[0]])
+  });
+};
+
+
+const pathValues2JSONGraphEnvelope = pathValues => {
+  const jsonGraph = pathValues.reduce((tree, pathValue) => pathValue2Tree(pathValue, tree), {});
+
+  const paths = collapse(pathValues.map(pathValue => pathValue.path));
+
+  return { jsonGraph, paths };
+};
+
+const expandPath = pathOrPathSet => {
+  return pathOrPathSet.reduce((paths, keyOrKeySet) => {
+    if (Array.isArray(keyOrKeySet)) {
+      // keySet, e.g. ['name', 'age']
+      return keyOrKeySet.reduce((expandedPaths, key) =>
+        [...expandedPaths, ...paths.map(path => [...path, key])],
+      []);
+    } else if (typeof keyOrKeySet === 'object') {
+      // range, e.g. [{ from: 1, to: 3 }]
+    }
+    // key
+    return paths.map(path => [...path, keyOrKeySet]);
+  }, [[]]);
+};
+
+const expandPaths = paths => {
+
+};
+
+
+const mergeTrees = (treeA, treeB) => {};
+
+
 
 const extractPathFromTree = (path, tree = {}) => {
   if (path.length === 1) {
@@ -24,34 +75,20 @@ const extractPathFromTree = (path, tree = {}) => {
   return { [path[0]]: extractPathFromTree(path.slice(1), tree[path[0]]) }
 }
 
+
 const extractPathsFromTree = (paths, tree) => {
-  paths.forEach(path)
+  // TODO - if extractPathFromTree could handle pathSets, rather than just paths,
+  // we could drop the expandPaths call and reduce the number of potential recursive
+  // passes through the graph
+  return expandPaths(paths).reduce((subTree, path) => {
+    return mergeTrees(subTree, extractPathFromTree(path));
+  }, {});
 };
-
-// TODO - make tail recursive?
-const pathValue2Tree = (pathValue, tree = {}) => {
-  if (pathValue.path.length === 1) {
-    return Object.assign(tree, {
-      [pathValue.path[0]]: pathValue.value
-    });
-  }
-
-  return Object.assign(tree, {
-    [pathValue.path[0]]: pathValue2Tree({ path: pathValue.path.slice(1), value: pathValue.value }, tree[pathValue.path[0]])
-  });
-};
-
-const pathValues2JSONGraphEnvelope = pathValues => {
-  const jsonGraph = pathValues.reduce((tree, pathValue) => pathValue2Tree(pathValue, tree), {});
-
-  const paths = collapse(pathValues.map(pathValue => pathValue.path));
-
-  return { jsonGraph, paths };
-};
-
-const expandPaths = paths => {};
 
 module.exports.walkTree = walkTree;
-module.exports.extractPathFromTree = extractPathFromTree;
 module.exports.pathValue2Tree = pathValue2Tree;
 module.exports.pathValues2JSONGraphEnvelope = pathValues2JSONGraphEnvelope;
+module.exports.expandPath = expandPath;
+module.exports.expandPaths = expandPaths;
+module.exports.extractPathFromTree = extractPathFromTree;
+module.exports.extractPathsFromTree = extractPathsFromTree;
